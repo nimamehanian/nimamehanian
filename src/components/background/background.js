cf927fbc8dce36a0a51652f51c2b688bb4f77446
Nima Mehanian <nima.mehanian@gmail.com>
2019-04-07 13:36:56 -0700

    Make background animate in response to mousemove

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
 import useInterval from 'utils/useInterval';
 
 import bg1 from 'images/bg1.jpg';
@@ -15,37 +16,48 @@ const BackgroundWrapper = styled.div`
   transform: scale(${({ scale }) => scale || 1});
 `;
 
-const Images = styled.div`
+const Images = styled(animated.div)`
   width: 110vw;
   height: 100vh;
   background-position: 64% center;
   background-repeat: no-repeat;
   background-size: cover;
-  background-image: url(${({ currentImage }) => currentImage});
   transition: background-image 0.25s ease-in-out;
-  transform: translate3d(${({ bgPosX }) => bgPosX || 0}px, ${({ bgPosY }) => bgPosY || 0}px, 0px) scale(1.15);
+  will-change: transform;
 `;
 
+const updateCoordinates = (x, y) => {
+  const [d0, d1, r0, r1] = [0, 100, 24, -24];
+  const position = c => Math.round((c - d0) * ((r1 - r0) / (d1 - d0)) + r0);
+  const getX = () => position((x / window.innerWidth) * 100);
+  const getY = () => position((y / window.innerHeight) * 100);
+  return [getX(), getY()];
+};
+
 function Background() {
-  const [[bgPosX, bgPosY], setCoordinates] = useState([0, 0]);
+  const [{ xy }, setCoords] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
   const [currentImgIdx, setCurrentImgIdx] = useState(0);
   const [images] = useState([bg1, bg2]);
-
-  function updateCoordinates({ clientX, clientY }) {
-    const [d0, d1, r0, r1] = [0, 100, 12, -12];
-    const position = c => Math.round((c - d0) * ((r1 - r0) / (d1 - d0)) + r0);
-    const getX = () => position((clientX / window.innerWidth) * 100);
-    const getY = () => position((clientY / window.innerHeight) * 100);
-    setCoordinates([getX(), getY()]);
-  }
 
   useInterval(() => {
     setCurrentImgIdx(currentImgIdx === images.length - 1 ? 0 : currentImgIdx + 1);
   }, 6000);
 
   return (
-    <BackgroundWrapper onMouseMove={e => updateCoordinates(e)}>
-      <Images currentImage={images[currentImgIdx]} bgPosX={bgPosX} bgPosY={bgPosY} />
+    <BackgroundWrapper
+      onMouseMove={({ clientX, clientY }) => (
+        setCoords({ xy: updateCoordinates(clientX, clientY) })
+      )}
+    >
+      <Images
+        style={{
+          backgroundImage: `url(${images[currentImgIdx]})`,
+          transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0) scale(1.15)`),
+        }}
+      />
     </BackgroundWrapper>
   );
 }
